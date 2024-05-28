1586ecf60d3508e107bf83fc0d0f77fec75e0360
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-05-27 22:42:12 -0300

    Commit 4

--- arquitetura/matricular-aluno.php
+++ arquitetura/matricular-aluno.php
@@ -1,6 +1,10 @@
 <?php
 
+use Alura\Arquitetura\Aplicacao\Aluno\MatricularAluno\MatricularAluno;
+use Alura\Arquitetura\Aplicacao\Aluno\MatricularAluno\MatricularAlunoDto;
 use Alura\Arquitetura\Dominio\Aluno\Aluno;
+use Alura\Arquitetura\Dominio\Aluno\LogDeAlunoMatriculado;
+use Alura\Arquitetura\Dominio\PublicadorDeEvento;
 use Alura\Arquitetura\Infra\Aluno\RepositorioDeAlunoEmMemoria;
 
 require_once 'vendor/autoload.php';
@@ -12,9 +16,13 @@ $ddd = $argv[4];
 $numero = $argv[5];
 
 
-$aluno = Aluno::comCpfNomeEEmail($cpf, $nome, $email)->adicionarTelefone($ddd, $numero);
+// $aluno = Aluno::comCpfNomeEEmail($cpf, $nome, $email)->adicionarTelefone($ddd, $numero);
+// $repositorio = new RepositorioDeAlunoEmMemoria();
+// $repositorio->adicionar($aluno);
 
-$repositorio = new RepositorioDeAlunoEmMemoria();
-$repositorio->adicionar($aluno);
+$publicador = new PublicadorDeEvento();
+$publicador->adicionarOuvinte(new LogDeAlunoMatriculado());
+$useCase = new MatricularAluno(new RepositorioDeAlunoEmMemoria(), $publicador);
+$useCase->executa(new MatricularAlunoDto($cpf, $nome, $email));
 
 // php matricular-aluno.php "123.456.789-10" "Vinicius Dias" "email@exemple.com" "24" "999999999"
---
+++ arquitetura/src/Dominio/Selo/Selo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Alura\Arquitetura\Dominio\Selo;
+
+use Alura\Arquitetura\Dominio\CPF;
+
+class Selo
+{
+  private CPF $cpfAluno;
+  private string $nome;
+}
